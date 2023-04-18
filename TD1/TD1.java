36d12797d42116358cf1b95a620885096098e625
DJLGabJean <116765613+DJLGabJean@users.noreply.github.com>
2023-04-18 11:38:43 +0200

    Exercice 1 et 2 du TD1 finis

---
+++ TD1/TD1.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+
+// Exercice 1
+
+// class TD1 {
+//     public static void main(String[] args) {
+//         System.out.println("Hello World!");
+//     }
+// }
+
+
+// Exercice 2
+
+// class TD1 {
+//     public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in);
+//         System.out.println("Quel est vote nom?");
+//         String name = scanner.nextLine();
+
+//         System.out.println("Bienvenue dans la programme main " + name + "!");
+//         scanner.close();   
+//     }
+// }
+
+
+// Exercice 3
+
+
+
+
+
+
