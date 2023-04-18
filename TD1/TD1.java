b77d19ae9da530dceb8e45ae9c6237feaa9b0e53
DJLGabJean <116765613+DJLGabJean@users.noreply.github.com>
2023-04-18 12:02:27 +0200

    Exercice 3,4 et 5 finis

--- TD1/TD1.java
+++ TD1/TD1.java
@@ -26,6 +26,68 @@ import java.util.Scanner;
 
 // Exercice 3
 
+// class TD1 {
+//     public static void main (String[] args){
+//         Scanner scanner = new Scanner(System.in);
+//         System.out.println("Donner votre premier nombre: ");
+//         String num1 = scanner.nextLine();
+//         System.out.println("Donner votre deuxieme nombre: ");
+//         String num2 = scanner.nextLine();
+
+//         // (a + b + |a - b|) / 2
+//         System.out.println("Le maximum entre vos deux nombres est " + Math.max(Integer.parseInt(num1), Integer.parseInt(num2))); 
+//         scanner.close();
+//     }
+// }
+
+
+// Exercice 4
+
+// class TD1 {
+//     public static void main (String[] args){
+//         Scanner scanner = new Scanner(System.in);
+//         System.out.println("Donner un nombre : ");
+//         String num1 = scanner.nextLine();
+
+//         if (Integer.parseInt(num1) % 2 == 0){
+//             System.out.println("Votre nombre est pair");
+//         }
+//         else {
+//             System.out.println("Votre nombre est impair");
+//         }
+//         scanner.close();
+//     }
+// }
+
+// Exercice 5
+
+// class TD1 {
+//     public static void main (String[] args){
+//         Scanner scanner = new Scanner(System.in);
+//         System.out.println("Donner votre premier nombre: ");
+//         int num1 = scanner.nextInt();
+//         System.out.println("Donner votre deuxieme nombre: ");
+//         int num2 = scanner.nextInt();
+
+//         int temp = num1;
+//         num1 = num2;
+//         num2 = temp;
+
+//         System.out.println("Après un échange, votre premier nombre est : " + num1);
+//         System.out.println("Après un échange, votre deuxième nombre est : " + num2);
+//         scanner.close();
+//     }
+// }
+
+// Exercice 6
+
+
+
+
+
+
+
+
 
 
 
